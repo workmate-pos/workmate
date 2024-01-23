f04081ce38709e463f7d8a52d0a09d64bd5e7b33
Tim van Dam <timvandamcs@gmail.com>
2024-01-23 18:05:07 +0100

    Speed up payments -> batch labour

--- extensions/work-orders-pos/src/hooks/use-payment-handler.ts
+++ extensions/work-orders-pos/src/hooks/use-payment-handler.ts
@@ -1,7 +1,6 @@
 import { useExtensionApi } from '@shopify/retail-ui-extensions-react';
 import { useEffect, useRef, useState } from 'react';
 import { WorkOrder } from '@web/services/work-orders/types.js';
-import { Cart } from '@shopify/retail-ui-extensions';
 import { attributesToProperties } from '@work-orders/common/custom-attributes/mapping/index.js';
 import { WorkOrderOrderAttributesMapping } from '@work-orders/common/custom-attributes/mapping/work-order-order.js';
 import { WorkOrderOrderLineItemAttributesMapping } from '@work-orders/common/custom-attributes/mapping/work-order-order-line-item.js';
@@ -10,35 +9,20 @@ import { CreateWorkOrder } from '@web/schemas/generated/create-work-order.js';
 import { getLabourPrice } from '../create-work-order/labour.js';
 import { useSettingsQuery } from '@work-orders/common/queries/use-settings-query.js';
 import { useAuthenticatedFetch } from './use-authenticated-fetch.js';
-import { withResolvers } from '@teifi-digital/shopify-app-toolbox/promise';
 import { createGid, parseGid } from '@teifi-digital/shopify-app-toolbox/shopify';
 import { groupBy, sumMap } from '@teifi-digital/shopify-app-toolbox/array';
 
 const useCartRef = () => {
   const api = useExtensionApi<'pos.home.modal.render'>();
   const cartRef = useRef(api.cart.subscribable.initial);
-  const promiseRef = useRef(withResolvers<Cart['lineItems'][number]>());
 
   useEffect(() => {
     api.cart.subscribable.subscribe(cart => {
-      const newLineItem = cart.lineItems.find(
-        lineItem =>
-          !cartRef.current.lineItems.some(item => item.uuid === lineItem.uuid && item.quantity === lineItem.quantity),
-      );
-
-      if (newLineItem) {
-        promiseRef.current.resolve(newLineItem);
-        promiseRef.current = withResolvers();
-      }
-
       cartRef.current = cart;
     });
   }, []);
 
-  return {
-    cartRef,
-    getNewLineItem: () => promiseRef.current.promise,
-  };
+  return cartRef;
 };
 
 export type PaymentHandler = ReturnType<typeof usePaymentHandler>;
@@ -52,7 +36,7 @@ export const usePaymentHandler = () => {
   const fetch = useAuthenticatedFetch();
   const settingsQuery = useSettingsQuery({ fetch });
 
-  const { cartRef, getNewLineItem } = useCartRef();
+  const cartRef = useCartRef();
 
   const handlePayment = async ({ workOrder }: { workOrder: WorkOrder }) => {
     if (settingsQuery.isLoading) {
@@ -93,41 +77,28 @@ export const usePaymentHandler = () => {
     }
 
     const groupedLabour = groupBy(createWorkOrder.labour, labour => labour.name);
+    // rename labour such that every labour item has a unique name (must have a unique name to prevent line item merging)
+    const renamedLabour = Object.fromEntries(
+      Object.entries(groupedLabour).flatMap(([name, labour]) => labour.map((l, i) => [`${name} ${i + 1}`, l])),
+    );
 
-    for (const labour of Object.values(groupedLabour)) {
-      for (let i = 0; i < labour.length; i++) {
-        const labourItem = labour[i]!;
-        const price = getLabourPrice([labourItem]);
+    const addCustomSalePromises = [];
 
-        let title = labourItem.name;
+    for (const [title, labour] of Object.entries(renamedLabour)) {
+      const price = getLabourPrice([labour]);
 
-        if (labour.length > 1) {
-          // pos orders the cart from newest to oldest, so add (1) last
-          title += ` (${labour.length - i})`;
-        }
-
-        const addedLineItem = getNewLineItem();
-
-        await api.cart.addCustomSale({
+      addCustomSalePromises.push(
+        api.cart.addCustomSale({
           taxable: true,
           quantity: 1,
           title,
           price,
-        });
-
-        const { uuid } = await addedLineItem;
-        const attributes = getLabourLineItemAttributes(
-          labourItem,
-          settingsQuery.data.settings.labourLineItemSKU || undefined,
-        );
-        await api.cart.addLineItemProperties(uuid, attributes);
-      }
+        }),
+      );
     }
 
     const groupedLineItems = groupBy(createWorkOrder.lineItems, lineItem => lineItem.productVariantId);
 
-    // add product line items in parallel because `await api.cart.addLineItem(...)` takes 3 seconds !!!
-    // we cannot do labour in parallel because mapping from added line item to labour item is difficult
     const addLineItemPromises = [];
 
     for (const lineItems of Object.values(groupedLineItems)) {
@@ -141,25 +112,29 @@ export const usePaymentHandler = () => {
       addLineItemPromises.push(api.cart.addLineItem(parseGid(lineItem.productVariantId).id, quantity));
     }
 
+    await Promise.all(addCustomSalePromises);
     await Promise.all(addLineItemPromises);
 
-    // TODO: Also batch the other things because they all have a different name anyway
+    const addAttributePromises = [];
 
     for (const lineItem of cartRef.current.lineItems) {
-      if (!lineItem.variantId) {
-        continue;
+      if (lineItem.variantId) {
+        const productVariantId = createGid('ProductVariant', lineItem.variantId.toString());
+        const lineItemUuids = groupedLineItems[productVariantId]?.map(li => li.uuid);
+
+        if (!lineItemUuids) {
+          api.toast.show(`Could not find line item uuids for product variant ${productVariantId}`);
+          continue;
+        }
+
+        const attributes = getProductLineItemAttributes(lineItemUuids);
+        addAttributePromises.push(api.cart.addLineItemProperties(lineItem.uuid, attributes));
+      } else if (lineItem.title) {
+        const labourItem = renamedLabour[lineItem.title];
+        if (!labourItem) continue; // should be impossible
+        const attributes = getLabourLineItemAttributes(labourItem, settingsQuery.data.settings.labourLineItemSKU);
+        addAttributePromises.push(api.cart.addLineItemProperties(lineItem.uuid, attributes));
       }
-
-      const productVariantId = createGid('ProductVariant', lineItem.variantId.toString());
-      const lineItemUuids = groupedLineItems[productVariantId]?.map(li => li.uuid);
-
-      if (!lineItemUuids) {
-        api.toast.show(`Could not find line item uuids for product variant ${productVariantId}`);
-        continue;
-      }
-
-      const attributes = getProductLineItemAttributes(lineItemUuids);
-      await api.cart.addLineItemProperties(lineItem.uuid, attributes);
     }
 
     api.navigation.dismiss();
