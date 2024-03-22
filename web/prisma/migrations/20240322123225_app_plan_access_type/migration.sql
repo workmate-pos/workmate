31ea8c1165ae4062d49cd715a5c371fea465d3fe
Tim van Dam <timvandamcs@gmail.com>
2024-03-22 13:41:14 +0100

    drop default
    (cherry picked from commit 31da8f4d0695b6e3cc573927ca24e4b9939a1b7e)

--- web/prisma/migrations/20240322123225_app_plan_access_type/migration.sql
+++ web/prisma/migrations/20240322123225_app_plan_access_type/migration.sql
@@ -3,3 +3,4 @@ CREATE TYPE "AppPlanCustomAccessType" AS ENUM ('DEFAULT', 'TEST');
 
 -- AlterTable
 ALTER TABLE "AppPlanCustomAccess" ADD COLUMN     "type" "AppPlanCustomAccessType" NOT NULL DEFAULT 'DEFAULT';
+ALTER TABLE "AppPlanCustomAccess" ALTER COLUMN "type" DROP DEFAULT;
