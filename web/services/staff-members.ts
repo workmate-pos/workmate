865188f37c0eaa84b012f8ee4b79f8dd2a733f5a
Tim van Dam <timvandamcs@gmail.com>
2024-03-27 17:26:28 +0100

    fix employee list

--- web/services/staff-members.ts
+++ web/services/staff-members.ts
@@ -5,6 +5,7 @@ import { Graphql } from '@teifi-digital/shopify-app-express/services';
 import { hasReadUsersScope } from './shop.js';
 import { db } from './db/db.js';
 import { PaginationOptions } from '../schemas/generated/pagination-options.js';
+import { escapeLike } from './db/like.js';
 
 export async function getStaffMembersByIds(
   session: Session,
@@ -44,7 +45,9 @@ export async function getStaffMembersPage(
   const graphql = new Graphql(session);
 
   if (!(await hasReadUsersScope(graphql))) {
-    const employees = await db.employee.getPage({ shop: session.shop, query: paginationOptions.query });
+    const query = paginationOptions.query ? escapeLike(`%${paginationOptions.query}%`) : undefined;
+
+    const employees = await db.employee.getPage({ shop: session.shop, query });
 
     return {
       shop: {
