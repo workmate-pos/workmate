6fa80708a3325b10081f714af4fb6f53b79e1b57
Tim van Dam <timvandamcs@gmail.com>
2024-11-06 11:14:12 +0100

    show calculate usererrors in banner

--- common/src/queries/use-calculated-draft-order-query.ts
+++ common/src/queries/use-calculated-draft-order-query.ts
@@ -6,6 +6,15 @@ import { DiscriminatedUnionPick } from '../types/DiscriminatedUnionPick.js';
 import { WEEK_IN_MS } from '../time/constants.js';
 import { omit, pick } from '@teifi-digital/shopify-app-toolbox/object';
 
+export class CalculateWorkOrderError extends Error {
+  constructor(
+    message: string,
+    public readonly errors: { field: string[]; message: string }[],
+  ) {
+    super(message);
+  }
+}
+
 export const useCalculatedDraftOrderQuery = (
   {
     fetch,
@@ -33,7 +42,7 @@ export const useCalculatedDraftOrderQuery = (
   options?: Partial<
     UseQueryOptions<
       CalculateDraftOrderResponse,
-      unknown,
+      Error | CalculateWorkOrderError,
       CalculateDraftOrderResponse,
       (
         | string
@@ -66,6 +75,8 @@ export const useCalculatedDraftOrderQuery = (
       paymentTerms,
     ],
     queryFn: async () => {
+      let bodyText = '';
+
       const response = await fetch('/api/work-order/calculate-draft-order', {
         method: 'POST',
         body: JSON.stringify({
@@ -80,13 +91,42 @@ export const useCalculatedDraftOrderQuery = (
           paymentTerms,
         } satisfies CalculateWorkOrder),
         headers: { 'Content-Type': 'application/json' },
+      }).catch(error => {
+        if ('body' in error && 'response' in error) {
+          bodyText = typeof error.body === 'string' ? error.body : JSON.stringify(error.body);
+          return error.response;
+        }
+
+        throw error;
       });
 
-      if (!response.ok) throw new Error(await response.text());
+      bodyText ||= await response.text();
 
-      const calculateDraftOrderResponse: CalculateDraftOrderResponse = await response.json();
+      if (response.status === 400) {
+        try {
+          const { error, errors } = JSON.parse(bodyText);
+          throw new CalculateWorkOrderError(error, errors);
+        } catch (error) {
+          if (error instanceof SyntaxError) {
+            // not json - fallthrough
+          } else {
+            throw error;
+          }
+        }
+      }
+
+      if (!response.ok) throw new Error(bodyText);
+
+      const calculateDraftOrderResponse: CalculateDraftOrderResponse = JSON.parse(bodyText);
 
       return calculateDraftOrderResponse;
+    },
+    retry: (failureCount, error) => {
+      if (error instanceof CalculateWorkOrderError) {
+        return false;
+      }
+
+      return failureCount < 3;
     },
   });
 
