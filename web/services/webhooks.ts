71aa2a6e35d86ff2414fa6889f068e72dddbdd92
Tim van Dam <timvandamcs@gmail.com>
2024-11-20 11:58:22 +0100

    fix

--- web/services/webhooks.ts
+++ web/services/webhooks.ts
@@ -198,7 +198,7 @@ export default {
         ),
       ]).then(results => results.some(changed => changed));
 
-      if (changed.some(changed => changed)) {
+      if (changed) {
         // wait for the next webhook before syncing to save some query cost
         return;
       }
