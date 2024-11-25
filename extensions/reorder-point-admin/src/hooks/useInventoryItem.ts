a4a5a9e2026f724713cccb13726e6d567711b866
Tim van Dam <timvandamcs@gmail.com>
2024-11-25 16:43:44 +0100

    fix reorder point bugs, allow deletion, product block

--- extensions/reorder-point-admin/src/hooks/useInventoryItem.ts
+++ extensions/reorder-point-admin/src/hooks/useInventoryItem.ts
@@ -1,7 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 import { useApi } from '@shopify/ui-extensions-react/admin';
 import { ID } from '@teifi-digital/shopify-app-toolbox/shopify';
-import { TARGET } from '../BlockExtension.js';
 
 const VARIANT_QUERY = `#graphql
   query getVariant($id: ID!) {
@@ -13,26 +12,28 @@ const VARIANT_QUERY = `#graphql
   }
 `;
 
-interface VariantQueryResponse {
+type VariantQueryResponse = {
   productVariant: {
     inventoryItem: {
       id: ID;
     };
-  };
-}
+  } | null;
+};
 
-export function useInventoryItem(variantId: ID | undefined) {
-  const { query } = useApi(TARGET);
-
+export function useInventoryItem(
+  {
+    query,
+  }: ReturnType<typeof useApi<'admin.product-variant-details.block.render' | 'admin.product-details.block.render'>>,
+  variantId: ID | undefined,
+) {
   const { data, isLoading } = useQuery({
     queryKey: ['variant', variantId],
-    queryFn: async () => {
-      if (!variantId) return null;
-      const response = await query<VariantQueryResponse>(VARIANT_QUERY, {
-        variables: { id: variantId },
-      });
-      return response?.data?.productVariant.inventoryItem.id ?? null;
-    },
+    queryFn: !variantId
+      ? skipToken
+      : async () => {
+          const response = await query<VariantQueryResponse>(VARIANT_QUERY, { variables: { id: variantId } });
+          return response?.data?.productVariant?.inventoryItem.id ?? null;
+        },
     enabled: !!variantId,
   });
 
